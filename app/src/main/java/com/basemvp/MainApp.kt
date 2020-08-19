662af632dc8e5dc7c3621787c1d9fb8fd225ae18
wb <524323451@qq.com>
2020-08-19 14:20:07 +0800

    更新

--- app/src/main/java/com/basemvp/MainApp.kt
+++ app/src/main/java/com/basemvp/MainApp.kt
@@ -5,16 +5,21 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidFileProperties
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import org.koin.core.module.Module
 
 class MainApp : BaseAPP() {
     override fun initKoin() {
+        /**
+         * 所有模块的 总module 添加到这里
+         */
         val modules = arrayListOf<Module>().apply {
             addAll(mainDiList)
         }
         startKoin {
             // use AndroidLogger as Koin Logger - default Level.INFO
-            androidLogger()
+            // 使用默认的 default Level.INFO 在kotlin版本1.4.0会崩溃 NoSuchMethodError
+            androidLogger(Level.ERROR)
             // use the Android context given there
             androidContext(this@MainApp)
             // load properties from assets/koin.properties file
--- app/src/main/java/com/basemvp/MainDIList.kt
+++ app/src/main/java/com/basemvp/MainDIList.kt
@@ -2,6 +2,9 @@ package com.basemvp
 
 import com.basemvp.main.mvvm.mvvmDemoDi
 
+/**
+ * 对应模块的 koin module 添加到这里
+ */
 val mainDiList = arrayListOf(
     mvvmDemoDi
 )
