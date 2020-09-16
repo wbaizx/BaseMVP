4242855bd0b2097af04e225036569d61a61ef5f2
wb <524323451@qq.com>
2020-09-16 10:17:26 +0800

    更新

--- common/src/main/java/com/base/common/util/NumberUtil.kt
+++ common/src/main/java/com/base/common/util/NumberUtil.kt
@@ -8,6 +8,13 @@ import java.text.DecimalFormat
  * DecimalFormat 主要用于格式化数字
  *
  * BigDecimal 主要用于做对精度要求高的运算，比如涉及到钱的，初始化尽量使用字符串作为参数
+ *
+ * 四舍五入规则，考虑负值（一般不会为负）
+ * RoundingMode.UP      舍弃部分始终进 1
+ * RoundingMode.DOWN    舍弃部分直接截掉不进 1
+ * RoundingMode.CEILING 用于别人向我给钱，数值会偏大（负值反向表示给出去，偏大给的就少） 1.2 -> 2  -1.2 -> -1
+ * RoundingMode.FLOOR   用于我向别人给钱，数值会偏小（负值反向表示给进来，偏小给的就多） 1.8 -> 1  -1.8 -> -2
+ * RoundingMode.HALF_UP 标准四舍五入
  */
 
 /**
